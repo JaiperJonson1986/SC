a5106c84a9bb679cc5c6b16a8087418587405c01
Emmanuil <82332067+JaiperJonson1986@users.noreply.github.com>
2023-03-13 18:57:52 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+
+function TestPage()
+{
+    alert("How are you?")
+    var ans = confirm("Are you feel good?")
+    if (ans == true)
+    {
+        alert("So good!")
+        }
+    else
+    {
+        alert("Not good!")
+    }
+    var ans2 = prompt("Are like us?")
+    if(ans2 == "Yes")
+    {
+        alert("We like you too")
+    }
+    else if(ans2 == "No")
+    {
+        alert("We don't like you too")
+    }
+    else
+    {
+        alert("Ok")
+    }
+}
+    //TestPage()
